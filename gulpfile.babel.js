0897b948883c92a7b94157fd177002d7eb8d8e52
David Padbury <david@davidpadbury.com>
2015-08-05 21:54:30 +0000

    download just the fonticons we need from fontello.com

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,10 +1,57 @@
+import fs from 'fs';
+import path from 'path';
 import gulp from 'gulp';
 import del from 'del';
 import imagemin from 'gulp-imagemin';
 import jpegRecompress from 'imagemin-jpeg-recompress';
+import rp from 'request-promise';
+import request from 'request';
+import unzip from 'unzip';
+import mkdirp from 'mkdirp-promise';
 
 gulp.task('clean', (callback) => {
     del('dist', callback);
+});
+
+gulp.task('fetch-fonticons', async function() {
+    // get a session-id for this config
+    const sessionId = await rp.post({
+        url: 'http://fontello.com',
+        formData: {
+            config: fs.createReadStream(__dirname + '/app/fonts/config.json')
+        }
+    });
+
+    // ensure we have dirs to place unzipped content
+    await Promise.all([ mkdirp(__dirname + '/dist/css'), mkdirp(__dirname + '/dist/font') ]);
+
+    // download zip archive and extract required files
+    return new Promise((resolve, reject) => {
+        request(`http://fontello.com/${sessionId}/get`)
+            .pipe(unzip.Parse())
+            .on('entry', (entry) => {
+                // get extension without period
+                const ext = path.extname(entry.path).slice(1);
+                const use = ['css', 'eot', 'woff', 'ttf', 'svg'].includes(ext);
+
+                if (use) {
+                    const filename = path.basename(entry.path);
+
+                    if (ext === 'css') {
+                        // put in css dir
+                        entry.pipe(fs.createWriteStream(`${__dirname}/dist/css/${filename}`));
+                    } else {
+                        // put in fonts dir
+                        entry.pipe(fs.createWriteStream(`${__dirname}/dist/font/${filename}`));
+                    }
+                } else {
+                    // ignore
+                    entry.autodrain();
+                }
+            })
+            .on('finish', resolve)
+            .on('error', reject);
+    });
 });
 
 gulp.task('optimize-wallpapers', () => {
@@ -16,4 +63,4 @@ gulp.task('optimize-wallpapers', () => {
         .pipe(gulp.dest('dist/wallpapers'));
 });
 
-gulp.task('build', [ 'optimize-wallpapers' ]);
+gulp.task('build', [ 'optimize-wallpapers', 'fetch-fonticons' ]);
